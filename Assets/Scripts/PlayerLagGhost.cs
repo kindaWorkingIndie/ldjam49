8bb33d4c19349bf541384f81e26bd21e70531a44
Sashify <Sasch2010@live.de>
2021-10-02 17:52:01 +0200

    null check

--- Assets/Scripts/PlayerLagGhost.cs
+++ Assets/Scripts/PlayerLagGhost.cs
@@ -56,6 +56,7 @@ public class PlayerLagGhost : MonoBehaviour
 
     public void PushCommand(GhostCommand cmd)
     {
+        if (commandQueue == null) return;
         commandQueue.Enqueue(cmd);
     }
 
