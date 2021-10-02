8b61209a8657e39949ae33d70feca1cd77253bfd
Sashify <Sasch2010@live.de>
2021-10-02 11:29:16 +0200

    initial scripts

---
+++ Assets/Scripts/PingController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingController : MonoBehaviour
+{
+    public int currentPingLevel;
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+
+    public float walkSpeed = 6;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    private Rigidbody2D rb;
+
+    public PlayerLagGhost ghost;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (!ghost)
+        {
+            Debug.LogError("No ghost attached");
+        }
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        if (x != 0)
+        {
+            y = 0;
+        }
+        else if (y != 0)
+        {
+            x = 0;
+        }
+        moveInput = new Vector2(x, y);
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * walkSpeed;
+    }
+}
