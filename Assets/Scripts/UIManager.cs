5472e94f694a9de5230ca28faa672634eef15be3
Sashify <Sasch2010@live.de>
2021-10-02 13:00:09 +0200

    update canvas and _GLOBALS

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Image pingDisplayImage;
+    public Image nextPingImage;
 
     void Start()
     {
@@ -13,6 +14,11 @@ public class UIManager : MonoBehaviour
 
     void Update()
     {
+        if (PingController.Instance.pingQueue.Count == 0) return;
         pingDisplayImage.color = PingController.Instance.lag.color;
+        if (PingController.Instance.nextLag != null)
+        {
+            nextPingImage.color = PingController.Instance.nextLag.color;
+        }
     }
 }
