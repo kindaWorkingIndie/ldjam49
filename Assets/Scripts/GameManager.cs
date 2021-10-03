f74d63be8ebc1d49db98d8a186cbcbe3c2ab6dc5
Sashify <Sasch2010@live.de>
2021-10-03 02:12:12 +0200

    add checkpoint message

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -91,6 +91,7 @@ public class GameManager : MonoBehaviour
     public void SetCheckpoint(Transform checkpoint)
     {
         latestCheckpoint = checkpoint;
+        UIManager.Instance.ShowScreenHint("Checkpoint reached");
     }
 
 }
