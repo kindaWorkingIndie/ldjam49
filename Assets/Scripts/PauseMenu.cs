37aecef6975061bbb3550adabf68af4679aa719f
Sashify <Sasch2010@live.de>
2021-10-03 03:07:18 +0200

    add pausemenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _isPaused = false;
+    public bool isPaused { get { return _isPaused; } }
+    public GameObject visuals;
+
+    private static PauseMenu _instance;
+    public static PauseMenu Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        visuals.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPaused)
+            {
+                UnpauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        _isPaused = true;
+        visuals.SetActive(_isPaused);
+        Time.timeScale = 0;
+    }
+
+    public void UnpauseGame()
+    {
+        _isPaused = false;
+        visuals.SetActive(_isPaused);
+        Time.timeScale = 1;
+    }
+
+    public void RestartLevel()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(sceneName);
+        UnpauseGame();
+    }
+
+    public void QuitGame()
+    {
+        UnpauseGame();
+        Application.Quit();
+    }
+}
